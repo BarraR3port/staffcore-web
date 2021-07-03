6979665f558f05e897023f9032cf734e4a7b51ed
BarraR3port <barrar3port@gmail.com>
2021-07-03 07:14:26 -0400

    Getting closer to a final release

--- database/db.sql
+++ database/db.sql
@@ -177,16 +177,7 @@ CREATE TABLE IF NOT EXISTS sc_servers_settings
     NULL
     DEFAULT
     TRUE,
-    CONSTRAINT
-    fk_server_id_settings
-    FOREIGN
-    KEY
-(
-    serverId
-) REFERENCES sc_servers
-(
-    serverId
-)
+    CONSTRAINT fk_server_id_settings FOREIGN KEY( serverId) REFERENCES sc_servers(serverId)
     );
 INSERT INTO sc_servers_settings(public, serverId)
 VALUES (public =?, serverId = ?)
@@ -205,3 +196,29 @@ INSERT INTO sc_servers(owner, server, address, username, db, host, port, passwor
 
 
 ALTER TABLE sc_servers_settings change maxBans maxBans varchar(40);
+--
+--
+--
+-- CREATE TABLE Inventario(IdInventario int PRIMARY KEY AUTO_INCREMENT, Realizo VARCHAR(30) NOT NULL);
+-- CREATE TABLE Predios(IdPredio int NOT NULL AUTO_INCREMENT PRIMARY KEY , Predio varchar(50) NOT NULL, Zona varchar(30) NOT NULL, codigoinventario INT NOT NULL );
+-- CREATE TABLE Contratos(IdContrato int NOT NULL AUTO_INCREMENT, Contrato varchar(12) NOT NULL, CodigoPredio int NOT NULL, PRIMARY KEY (IdContrato), CONSTRAINT fk_cod_Predido FOREIGN KEY( CodigoPredio ) REFERENCES Predios(IdPredio) );
+-- CREATE TABLE Orden(idorden int NOT NULL AUTO_INCREMENT PRIMARY KEY, anoplanta int NOT NULL, superficie REAL NOT NULL, Bloque int NOT NULL, codigocontrato int NOT NULL, FOREIGN KEY (codigocontrato) REFERENCES Contratos(IdContrato) );
+-- CREATE TABLE Sitios(IdSitio int NOT NULL AUTO_INCREMENT PRIMARY KEY,Sitio int NOT NULL, CodigoOrden int NOT NULL);
+-- CREATE TABLE Arboles (IdArbol int NOT NULL AUTO_INCREMENT, Fuente varchar(10) NOT NULL, Especie varchar(20) NOT NULL, Edad REAL NOT NULL, NumArbol int NOT NULL, Diametro REAL NOT NULL, Altura REAL NOT NULL, CodigoSitio int NOT NULL, FechaPlan int NOT NULL , PRIMARY KEY (IdArbol), FOREIGN KEY (CodigoSitio) REFERENCES Sitios(IdSitio));
+-- CREATE TABLE Usuarios(Clave VARCHAR(10) NOT NULL, Nombre VARCHAR(20) NOT NULL, ApellidoP VARCHAR(20), ApellidoM VARCHAR(20), Tipo VARCHAR(12));
+-- CREATE TABLE Persona(idPersona INT PRIMARY KEY AUTO_INCREMENT NOT NULL, Nombres VARCHAR(45), ApePaterno VARCHAR(45), ApeMaterno VARCHAR(45), FechaNacimiento DATETIME, Email VARCHAR(45), Direccion VARCHAR(45), Telefono VARCHAR(45));
+--
+-- CREATE TABLE Perfil (idPerfil INT PRIMARY KEY AUTO_INCREMENT NOT NULL, Nombre VARCHAR(45), Descripcion VARCHAR(45));
+--
+-- RENAME TABLE Usuarios TO Usuario;
+-- ALTER TABLE Usuario CHANGE Nombre NombreUsuario varchar(45) NOT NULL;
+-- ALTER TABLE Usuario CHANGE Clave Contraseniha varchar(45) NOT NULL;
+-- ALTER TABLE Usuario DROP COLUMN Tipo;
+-- ALTER TABLE Usuario DROP COLUMN ApellidoP;
+-- ALTER TABLE Usuario DROP COLUMN ApellidoM;
+-- ALTER TABLE Usuario ADD Estado varchar(45) NOT NULL;
+-- ALTER TABLE Usuario ADD IdUsuario INT NOT NULL PRIMARY KEY AUTO_INCREMENT;
+-- ALTER TABLE Usuario ADD IdPersona INT NOT NULL;
+-- ALTER TABLE Usuario ADD FOREIGN KEY (IdPersona) REFERENCES Persona(idPersona);
+-- ALTER TABLE Usuario ADD IdPerfil INT NOT NULL;
+-- ALTER TABLE Usuario ADD FOREIGN KEY (IdPerfil) REFERENCES Perfil(IdPerfil);
